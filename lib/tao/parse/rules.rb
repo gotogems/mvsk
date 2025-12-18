7f413f4559373c743dbe896ebafc6e15ec914a0d
oneureka <65535758+oneureka@users.noreply.github.com>
2025-12-18 11:53:18 +0800

    The Tao programming language

--- lib/tao/parse/rules.rb
+++ lib/tao/parse/rules.rb
@@ -1,7 +1,40 @@
 module Tao
   module Parse
+    Rule = Struct.new(:prefix, :infix)
+
     module Rules
-      RULES = {}
+      RULES = {
+        Token::Plus       => Rule.new(nil, :parse_infix),
+        Token::Minus      => Rule.new(:parse_unary, :parse_infix),
+        Token::Star       => Rule.new(nil, :parse_infix),
+        Token::Slash      => Rule.new(nil, :parse_infix),
+        Token::Modulo     => Rule.new(nil, :parse_infix),
+        Token::Greater    => Rule.new(nil, :parse_infix),
+        Token::GreaterEq  => Rule.new(nil, :parse_infix),
+        Token::Less       => Rule.new(nil, :parse_infix),
+        Token::LessEq     => Rule.new(nil, :parse_infix),
+        Token::Equal      => Rule.new(nil, :parse_infix),
+        Token::EqualEq    => Rule.new(nil, :parse_infix),
+        Token::BangEq     => Rule.new(nil, :parse_infix),
+        Token::And        => Rule.new(nil, :parse_infix),
+        Token::Or         => Rule.new(nil, :parse_infix),
+        Token::Not        => Rule.new(:parse_unary),
+        Token::PipeOp     => Rule.new(nil, :parse_infix),
+        Token::Dot        => Rule.new(nil, :parse_infix),
+        Token::LSquare    => Rule.new(:parse_array, :parse_index),
+        Token::LBrace     => Rule.new(:parse_prefix),
+        Token::LParen     => Rule.new(:parse_group, :parse_call),
+        Token::Identifier => Rule.new(:parse_identifier),
+        Token::String     => Rule.new(:parse_literal),
+        Token::Int        => Rule.new(:parse_literal),
+        Token::Float      => Rule.new(:parse_literal),
+        Token::Bool       => Rule.new(:parse_literal),
+        Token::None       => Rule.new(:parse_literal),
+        Token::If         => Rule.new(:parse_if),
+        Token::Match      => Rule.new(:parse_match),
+        Token::Fun        => Rule.new(:parse_func),
+        Token::Self       => Rule.new(:parse_self)
+      }.freeze
     end
   end
 end
